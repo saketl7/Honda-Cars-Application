1c06dafcce4f644f8779d665417c13a10ce9e77a
saketlakhotia <saket.lakhotia17>
2021-09-03 23:15:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/HondaCar/admin_login.java
@@ -0,0 +1,14 @@
+package com.example.HondaCar;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class admin_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_login);
+    }
+}
---
+++ app/src/main/java/com/example/HondaCar/profile.java
@@ -0,0 +1,14 @@
+package com.example.HondaCar;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
