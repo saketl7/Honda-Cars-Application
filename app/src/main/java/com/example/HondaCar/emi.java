f3b72977d071bf634f289b47f06bea6560ec6252
saketlakhotia17 <saket.lakhotia17@gmail.com>
2022-02-18 11:11:18 +0530

    Menu

--- app/src/main/java/com/example/HondaCar/emi.java
+++ app/src/main/java/com/example/HondaCar/emi.java
@@ -3,16 +3,29 @@ package com.example.HondaCar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class emi extends AppCompatActivity {
 
+    private EditText principal, interest, years, downpayment;
+    private TextView monthlyEMI;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emi);
+
+        principal = (EditText)findViewById(R.id.principal_amount);
+        downpayment=(EditText)findViewById(R.id.down_payment);
+        interest = (EditText)findViewById(R.id.interest_rate);
+        years = (EditText)findViewById(R.id.total_years);
+        monthlyEMI = (TextView)findViewById(R.id.calculated_emi);
 
 
         ImageView Icon = findViewById(R.id.back);
@@ -24,4 +37,24 @@ public class emi extends AppCompatActivity {
             }
         });
     }
+
+    public void calculatePayments(View clickedButton){
+
+        double principalAmount = Double.parseDouble(principal.getText().toString());
+        double interestRate = Double.parseDouble(interest.getText().toString());
+        double totalYears = Double.parseDouble(years.getText().toString());
+        double downpment = Double.parseDouble(downpayment.getText().toString());
+        double p = principalAmount;
+        double d = downpment;
+        double r2 = p - d;
+        double r = (interestRate/12)/100;
+        double n = totalYears * 12;
+
+        double r1 = Math.pow((1+r), n);
+        double emi = r2 * ((r*r1)/(r1-1));
+
+        monthlyEMI.setText(new DecimalFormat(".00").format(emi));
+
+    }
+
 }
--- app/src/main/java/com/example/HondaCar/test.java
+++ app/src/main/java/com/example/HondaCar/test.java
@@ -3,16 +3,28 @@ package com.example.HondaCar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class test extends AppCompatActivity {
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        webView = (WebView) findViewById(R.id.webview1);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.hondacarindia.com/request-test-drive");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
 
         ImageView Icon = findViewById(R.id.back);
         Icon.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +35,14 @@ public class test extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
